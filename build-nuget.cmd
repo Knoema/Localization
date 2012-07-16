9cc4871a0474845a470d73d213a43bfc17a7282d
vbougay <vbougay@knoema.com>
2012-07-16 21:13:37 +0600

    KN-2992 Fixed NuGet specs

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -3,5 +3,6 @@ if exist NuGet. (
 ) else (
 	mkdir NuGet
 )
-nuget pack -Build Core\Knoema.Localization.Core.csproj -Symbols -OutputDirectory .\NuGet -Prop Configuration=Release
-nuget pack -Build Mvc\Knoema.Localization.Mvc.csproj -Symbols -OutputDirectory .\NuGet -Prop Configuration=Release
+nuget pack -Build Core\Knoema.Localization.Core.csproj -OutputDirectory .\NuGet -Prop Configuration=Release
+nuget pack -Build EFProvider\Knoema.Localization.EFProvider.csproj -OutputDirectory .\NuGet -Prop Configuration=Release
+nuget pack -Build Mvc\Knoema.Localization.Mvc.csproj -OutputDirectory .\NuGet -Prop Configuration=Release
