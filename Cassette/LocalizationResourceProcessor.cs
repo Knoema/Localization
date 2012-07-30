e6847e7f2cb45bd7cf1378aa1777ef8bbbcaf937
vbougay <vbougay@knoema.com>
2012-07-30 16:19:10 +0600

    KN-2992 Cassette module

---
+++ Cassette/LocalizationResourceProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cassette.BundleProcessing;
+using Cassette.Configuration;
+using Cassette.HtmlTemplates;
+using Cassette.Scripts;
+
+namespace Knoema.Localization.Cassette
+{
+	public class LocalizationResourceProcessor : IBundleProcessor<ScriptBundle>, IBundleProcessor<HtmlTemplateBundle>
+	{
+		public void Process(ScriptBundle bundle, CassetteSettings settings)
+		{
+			foreach (var asset in bundle.Assets)
+				asset.AddAssetTransformer(new LocalizationResourceTransformer(asset.SourceFile.FullPath));
+		}
+
+		public void Process(HtmlTemplateBundle bundle, CassetteSettings settings)
+		{
+			foreach (var asset in bundle.Assets)
+				asset.AddAssetTransformer(new LocalizationResourceTransformer(asset.SourceFile.FullPath));
+		}
+	}
+}
---
+++ Cassette/LocalizationResourceTransformer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using Cassette;
+
+namespace Knoema.Localization.Cassette
+{
+	public class LocalizationResourceTransformer : IAssetTransformer
+	{
+		private string _path;
+		public LocalizationResourceTransformer(string path)
+		{
+			_path = path;
+		}
+
+		public Func<Stream> Transform(Func<Stream> openSourceStream, IAsset asset)
+		{
+			return delegate
+			{
+				using (var input = new StreamReader(openSourceStream()))
+				{
+					var stream = new MemoryStream();
+					var writer = new StreamWriter(stream);
+
+					var regex = new Regex("__R\\([\"']{1}.*?[\"']{1}\\)", RegexOptions.Compiled);
+					var script = regex.Replace(input.ReadToEnd(), delegate(Match match)
+					{
+						var resource = match.Value
+							.Remove(match.Value.LastIndexOf(")"), 1)
+							.Replace("__R", "$.localize");
+
+						resource = string.Format("{0}, \"{1}\")", resource, _path);
+
+						return Path.GetExtension(_path) == ".htm" ? "${" + resource + "}" : resource;
+					});
+
+					writer.Write(script);
+					writer.Flush();
+					stream.Position = 0;
+
+					return stream;
+				}
+			};
+		}
+	}
+}
