03edaeb6f1f339a863d22f596ff2d4585adc4261
polyakova <epolyakova@knoema.com>
2012-10-01 10:21:31 +0600

    Entity Framework is updated to v. 5.0.0

--- EFProvider/LocalizedObject.cs
+++ EFProvider/LocalizedObject.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Knoema.Localization.EFProvider
 {
