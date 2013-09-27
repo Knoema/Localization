8d57e457e70d5a191c880ca3963a367587984065
polyakova <epolyakova@knoema.com>
2013-09-27 19:20:04 +0600

    KN-6865 Added override for methods R, R2 and changed WebViewPage<TModel> class

--- Mvc/Mvc/LocalizedWebViewPage.cs
+++ Mvc/Mvc/LocalizedWebViewPage.cs
@@ -9,19 +9,30 @@ using System.Web.UI;
 
 using Knoema.Localization;
 using Knoema.Localization.Web;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Knoema.Localization.Mvc
 {
 	public abstract class LocalizedWebViewPage : WebViewPage
 	{
-		public string R(string text, CultureInfo culture = null, params object[] formatterArguments)
+		public string R(string text, params object[] formatterArguments)
 		{
-			return this.T(text, culture, formatterArguments);
+			return this.Translate(text, formatterArguments: formatterArguments);
 		}
 
-		public HtmlString R2(string text, CultureInfo culture = null, params object[] formatterArguments)
+		public string R(string text, CultureInfo culture, params object[] formatterArguments)
 		{
-			return this.T2(text, culture, formatterArguments);
+			return this.Translate(text, culture, formatterArguments);
+		}
+
+		public HtmlString R2(string text, params object[] formatterArguments)
+		{
+			return this.TranslateMarkup(text, formatterArguments: formatterArguments);
+		}
+
+		public HtmlString R2(string text, CultureInfo culture, params object[] formatterArguments)
+		{
+			return this.TranslateMarkup(text, culture, formatterArguments);
 		}
 
 		public MvcHtmlString RenderLocalizationIncludes(bool admin)
@@ -30,27 +41,57 @@ namespace Knoema.Localization.Mvc
 		}
 	}
 
-	public abstract class LocalizedWebViewPage<TModel> : WebViewPage<TModel>
+	public abstract class LocalizedWebViewPage<TModel> : LocalizedWebViewPage
 	{
-		public string R(string text, CultureInfo culture = null, params object[] formatterArguments)
+		private ViewDataDictionary<TModel> _viewData;
+
+		public new AjaxHelper<TModel> Ajax { get; set; }
+
+		public new HtmlHelper<TModel> Html { get; set; }
+
+		public new TModel Model
 		{
-			return this.T(text, culture, formatterArguments);
+			get
+			{ 
+				return ViewData.Model;
+			}
 		}
 
-		public HtmlString R2(string text, CultureInfo culture = null, params object[] formatterArguments)
+		[SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "This is the mechanism by which the ViewPage gets its ViewDataDictionary object.")]
+		public new ViewDataDictionary<TModel> ViewData
 		{
-			return this.T2(text, culture, formatterArguments);
+			get
+			{
+				if (_viewData == null)				
+					SetViewData(new ViewDataDictionary<TModel>());				
+
+				return _viewData;
+			}
+			set 
+			{ 
+				SetViewData(value); 
+			}
 		}
 
-		public MvcHtmlString RenderLocalizationIncludes(bool admin)
+		public override void InitHelpers()
 		{
-			return MvcHtmlString.Create(LocalizationHandler.RenderIncludes(admin, LocalizationManager.Instance.GetScope()));
+			base.InitHelpers();
+
+			Ajax = new AjaxHelper<TModel>(ViewContext, this);
+			Html = new HtmlHelper<TModel>(ViewContext, this);
+		}
+
+		protected override void SetViewData(ViewDataDictionary viewData)
+		{
+			_viewData = new ViewDataDictionary<TModel>(viewData);
+
+			base.SetViewData(_viewData);
 		}
 	}
 
 	internal static class WebViewPageExtensions
 	{
-		internal static string T(this WebViewPage page, string text, CultureInfo culture = null, params object[] formatterArguments)
+		internal static string Translate(this WebViewPage page, string text, CultureInfo culture = null, params object[] formatterArguments)
 		{
 			var translation = Translate(page.VirtualPath, text, culture: culture);
 
@@ -60,7 +101,7 @@ namespace Knoema.Localization.Mvc
 			return formatterArguments.Length == 1 ? translation.FormatWith(formatterArguments[0]) : string.Format(translation, formatterArguments);
 		}
 
-		internal static HtmlString T2(this WebViewPage page, string text, CultureInfo culture = null, params object[] formatterArguments)
+		internal static HtmlString TranslateMarkup(this WebViewPage page, string text, CultureInfo culture = null, params object[] formatterArguments)
 		{
 			var translation = Translate(page.VirtualPath, text, true, culture);
 
