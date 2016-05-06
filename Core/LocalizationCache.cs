811e9a1a11b58622a348d7e4aefd06b9f6daa4cc
afilippov <afilippov@knoema.com>
2016-05-06 11:14:31 +0500

    KN-15608 MemCached -> Redis

--- Core/LocalizationCache.cs
+++ Core/LocalizationCache.cs
@@ -7,50 +7,49 @@ namespace Knoema.Localization
 	{
 		private static ConcurrentDictionary<string, object> _memoryCache = new ConcurrentDictionary<string, object>();
 		static ILocalizationCache _cache;
+
+		const string LocalizationChannel = "localization";
 		public static void Initialize(ILocalizationCache cache)
 		{
 			_cache = cache;
+			_cache.Subscribe(LocalizationChannel, OnClearKey);
 		}
 
 		public static T Get<T>(string key) where T : class
 		{
-			var timeStamp = (DateTime?)_cache.Get("stamp_" + key);
-			if (!timeStamp.HasValue)
-				return null;
-			object localTimeStamp;
-			if (!_memoryCache.TryGetValue("stamp_" + key, out localTimeStamp))
-				localTimeStamp = null;
-			T value = null;
-			if (localTimeStamp == null || (DateTime?)localTimeStamp < timeStamp)
+			object objValue;
+			_memoryCache.TryGetValue(key, out objValue);
+			var value = (T)objValue;
+			if (value == null)
 			{
 				value = (T)_cache.Get(key);
 				if (value != null)
-				{
 					_memoryCache[key] = value;
-					_memoryCache["stamp_" + key] = timeStamp;
-				}
 			}
-			else
-				value = (T)_memoryCache[key];
 			return value;
 		}
 
 		public static void Insert<T>(string key, T value)
 		{
-			var timeStamp = DateTime.UtcNow;
-			_cache.Set("stamp_" + key, timeStamp, timeStamp.AddDays(1));
-			_cache.Set(key, value, timeStamp.AddDays(1));
-			_memoryCache["stamp_" + key] = timeStamp;
+			_cache.Set(key, value, DateTime.UtcNow.AddDays(1));
 			_memoryCache[key] = value;
+			_cache.Publish(LocalizationChannel, key);
 		}
 
 		public static void Remove(string key)
 		{
 			_cache.Remove(key);
-			_cache.Remove("stamp_" + key);
 			object value;
 			_memoryCache.TryRemove(key, out value);
-			_memoryCache.TryRemove("stamp_" + key, out value);
+			_cache.Publish(LocalizationChannel, key);
+		}
+
+		static void OnClearKey(string channel, string key)
+		{
+			if (channel != LocalizationChannel)
+				return;
+			object value;
+			_memoryCache.TryRemove(key, out value);
 		}
 	}
 }
--- Sample/HttpCache.cs
+++ Sample/HttpCache.cs
@@ -17,32 +17,53 @@ namespace Sample
 			_cache = HttpRuntime.Cache;
 		}
 
-		public object Add(string key, object entry, DateTime utcExpiry)
+		public object Add(string key, object entry, DateTime utcExpiry, string region = null)
 		{
 			return _cache.Add(key, entry, null, utcExpiry, Cache.NoSlidingExpiration, CacheItemPriority.Default, null);
 		}
 
-		public object Get(string key)
+		public object Get(string key, string region = null)
 		{
 			return _cache.Get(key);
 		}
 
-		public void Remove(string key)
+		public void Remove(string key, string region = null)
 		{
 			_cache.Remove(key);
 		}
 
-		public void Set(string key, object entry, DateTime utcExpiry)
+		public void Set(string key, object entry, DateTime utcExpiry, string region = null)
 		{
 			_cache.Insert(key, entry, null, utcExpiry, Cache.NoSlidingExpiration, CacheItemPriority.Default, null);
 		}
 
-		public void Clear()
+		public void Clear(string region = null)
 		{
 			var items = _cache.GetEnumerator();
 
 			while (items.MoveNext())
 				_cache.Remove(items.Key.ToString());
+		}
+
+		const string ChannelPrefix = "channel_";
+		public void Subscribe(string channel, Action<string, string> callback)
+		{
+			var channelKey = ChannelPrefix + channel;
+			var callbacks = (List<Action<string, string>>)_cache.Get(channelKey);
+			if (callbacks == null)
+				callbacks = new List<Action<string,string>>();
+			callbacks.Add(callback);
+			_cache.Insert(channelKey, callbacks);
+		}
+
+		public void Publish(string channel, string message)
+		{
+			var channelKey = ChannelPrefix + channel;
+			var callbacks = (List<Action<string, string>>)_cache.Get(channelKey);
+			if (callbacks == null)
+				return;
+			foreach(var callback in callbacks)
+				callback(channel, message);
 		}
 	}
 }
